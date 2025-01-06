69f56906482a93d1c8ae401d95e84cadbc909799
roberty <clsrntkdgh@gmail.com>
2025-01-06 12:07:19 +0900

    method overlording

--- ch04/src/sub3/MethodOverloadingTest.java
+++ ch04/src/sub3/MethodOverloadingTest.java
@@ -1,10 +1,43 @@
 package sub3;
 
+/*
+ *	날짜 : 2025/01/06 
+ *	이름 : 우상호
+ *	내용 : JAVA 메서드 오버로딩 실습하기
+ */
+
 public class MethodOverloadingTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int r1 = add(3);
+		int r2 = add(1, 2);
+		double r3 = add(0.1, 2.1);
+		String r4 = add("Hello ", "World!");
 
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println(r3);
+		System.out.println(r4);
 	}
+	
+	// 오버로딩 메서드 정의
+	public static int add(int a) {
+		return a + a;
+	}
+	
+	public static int  add(int a, int b) {
+		return a + b;
+	}
+	
+	public static double add(double a , double b) {
+		return a + b;
+	}
+	
+	public static String add(String a , String b) {
+		return a + b;
+	}
+	
+	
 
 }
