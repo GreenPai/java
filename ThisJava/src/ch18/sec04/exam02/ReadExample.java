4fc50c69f0d3a8456c7cfc7c5048a7f6d5d7c374
roberty <clsrntkdgh@gmail.com>
2025-01-16 16:49:40 +0900

    입출력 교재 내용

---
+++ ThisJava/src/ch18/sec04/exam02/ReadExample.java
@@ -0,0 +1,48 @@
+package ch18.sec04.exam02;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class ReadExample {
+	public static void main(String[] args) {
+		
+		
+		// 문자씩 열기
+		try {
+			Reader reader = null;
+			reader = new FileReader("C:/Temp/test.txt");
+			
+			while(true) {
+				int data = reader.read();
+				if(data == -1) break;
+				System.out.println((char)data);
+			}
+			
+			reader.close();
+			System.out.println();
+			
+			//문자 배열로 읽기
+			reader = new FileReader("C:/Temp/test.txt");
+			char[] data = new char[100];
+			while(true) {
+				int num = reader.read(data);
+				if(num==-1) break;
+				for(int i=0; i<num; i++) {
+					System.out.println(data[i]);
+				}
+			}
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
