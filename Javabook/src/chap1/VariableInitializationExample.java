31f6143668c27946aa33bd05f514e294aa91e989
roberty <clsrntkdgh@gmail.com>
2025-09-29 22:42:11 +0900

    자바

---
+++ Javabook/src/chap1/VariableInitializationExample.java
@@ -0,0 +1,14 @@
+package chap1;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        // value 선언
+        int value = 0;
+        
+        // 연산 결과를 변수 result의 초기갑스로 대입
+        int result = value + 10;
+        
+        //변수 result 값을 읽고 콘솔에 출력
+        System.out.println(result);
+    }
+}
