b718e681f1ed65fba4766ae7ee5a904d46684357
roberty <clsrntkdgh@gmail.com>
2025-10-18 23:56:23 +0900

    mvc 위치 이동

---
+++ 인프런/김영한 강의자료/MVC1/src/main/java/hello/servlet/ServletApplication.java
@@ -0,0 +1,24 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+/*
+* @ServletComponentScan
+* 스프링 부트는 서블릿을 직접 등록해서 사용할 수 있도록
+* @ServletComponentScan을 지원한다.
+* */
+
+@ServletComponentScan //서블릿 자동 등록
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
