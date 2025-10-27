171789b16c26aeacb13fb5568e44d3279fc5413d
roberty <clsrntkdgh@gmail.com>
2025-10-27 23:47:35 +0900

    jdbc 템플릿의 활용

--- inflearn/김영한/db2/source/itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ inflearn/김영한/db2/source/itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -8,8 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Import(JdbcTemplateV1Config.class)
+//@Import(JdbcTemplateV1Config.class)
+//@Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
 // @Import(MemoryConfig.class)
+
+/**
+ * @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+ * 스프링 부트 애플리케이션의 시작점임을 표시.
+ * 기본적으로 @SpringBootApplication이 선언된 패키지(hello.itemservice) 하위 전체를 스캔하지만, 여기서는 hello.itemservice.web만 스캔하도록 지정했어요.
+ */
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
