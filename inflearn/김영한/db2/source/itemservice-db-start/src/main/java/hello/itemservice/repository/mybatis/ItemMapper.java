05095c4caed0b239f23a5f7c225bacc9e703af80
roberty <clsrntkdgh@gmail.com>
2025-12-08 23:49:13 +0900

    mybatis 주석 추가

--- inflearn/김영한/db2/source/itemservice-db-start/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ inflearn/김영한/db2/source/itemservice-db-start/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -9,6 +9,13 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * MyBatis 스프링 연동 모듈 -> @Mapper 조회
+ * 해당 인터페이스를 발견하면 ItemMapper 인터페이스 구현체를 만든다.(proxy)
+ * 생성된 동적 프록시 객체를 스프링 빈으로 등록
+ * --> ItemRepositoryTest에서 프록시가 생성되는 것을 확인할 수 있음.
+ */
+
 @Mapper
 public interface ItemMapper {
 
