e1e5444840c49dc0bd8694a82f6556df5fea7785
roberty <clsrntkdgh@gmail.com>
2025-12-09 23:57:22 +0900

    jpa

--- inflearn/김영한/db2/source/itemservice-db-start/src/main/java/hello/itemservice/domain/Item.java
+++ inflearn/김영한/db2/source/itemservice-db-start/src/main/java/hello/itemservice/domain/Item.java
@@ -1,12 +1,17 @@
 package hello.itemservice.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity // -> JPA가 사용하는 객체라는 뜻. 애노테이션이 있어야 JPA 인식 가능하다.
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 객체의 필드를 테이블의 컬럼로 매핑한다.
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
