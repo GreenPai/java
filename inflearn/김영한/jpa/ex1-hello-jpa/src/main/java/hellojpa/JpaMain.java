fcb3e669a58f771f9f52d1c9cb2c0c23c36adff6
roberty <clsrntkdgh@gmail.com>
2026-02-03 19:57:02 +0900

    JPA 영속성 컨텍스트

--- inflearn/김영한/jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ inflearn/김영한/jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -77,26 +77,113 @@ public class JpaMain {
          */
 
         // 회원 수정
+//        try{
+//
+//            // Member findMember = em.find(Member.class, 3L);
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                            .setFirstResult(0)
+//                                    .setMaxResults(10)
+//                                            .getResultList();
+//
+//            for(Member member : result){
+//                System.out.println(member.getName());
+//            }
+//
+//            tx.commit();
+//        }catch(Exception e){
+//            tx.rollback();
+//        }finally {
+//            em.close();
+//        }
+//
+//
+//
+
+        // =
+
+        /**
+         * 캐싱 테스트
+         * em.persist를 할 때 1차 캐시에 값이 저장되고 다시 호출할 때 findMember은 1차 캐시의 값을 들고온다.
+         * insert하는 쿼리는 과정이 다 끝난 후 트랜잭션되고 들어간다.
+         */
+
+//
+//        try{
+//
+//            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            //영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member); // 1차 캐시 저장
+//            System.out.println("=== AFTER ===");
+//
+//            Member findMember = em.find(Member.class, 101L); // 1차 캐시에 있는 Member을 가져옴.
+//
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+//
+//            tx.commit();
+//
+//        }catch(Exception e){
+//            tx.rollback();
+//        }finally {
+//            em.close();
+//        }
+
+        /**
+         * 영속 엔티티의 동일성 보장
+         * 조회를 2번 햇을 때 member1이 캐시에 저장되기 때문에
+         * findMember2에서는 캐시에 저장된 값을 호출한다.
+         *
+         * 그렇기 때문에 조회 쿼리는 한번만 나온다.
+         * result = " + (findMember1 == findMember2) ----> true
+         *
+         */
+
+//        try{
+//
+//            // 영속
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//
+//            System.out.println("result = " + (findMember1 == findMember2));
+//
+//            tx.commit();
+//
+//        }catch(Exception e){
+//            tx.rollback();
+//        }finally {
+//            em.close();
+//        }
+
+
+        /**
+         * 수정을 하는 경우에 따로 persist나 update 해주지 않아도
+         * 조회한 값을 수정하는 경우에
+         * 마지막에 update 쿼리가 실행이 된다.
+         *
+         */
+
         try{
 
-            // Member findMember = em.find(Member.class, 3L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                            .setFirstResult(0)
-                                    .setMaxResults(10)
-                                            .getResultList();
+            // 영속
+            Member member1 = em.find(Member.class, 150L);
+            member1.setName("ZZZZ");
 
-            for(Member member : result){
-                System.out.println(member.getName());
-            }
+            System.out.println("==================");
 
             tx.commit();
+
         }catch(Exception e){
             tx.rollback();
         }finally {
             em.close();
         }
 
-        
+
         emf.close();
     }
 }
