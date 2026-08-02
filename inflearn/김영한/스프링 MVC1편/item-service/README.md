07777ec3f3ef6117ed24bc064d3584ef0170999a
roberty <clsrntkdgh@gmail.com>
2026-08-02 16:50:06 +0900

    스프링 MVC

---
+++ inflearn/김영한/스프링 MVC1편/item-service/README.md
@@ -0,0 +1,32 @@
+<h1>프로젝트 정보</h1><br>
+
+<h2>프로젝트 선택</h2> <br>
+ <h3>Project: Gradle Project </h3>
+ - Language: Java <br>
+ - Spring Boot: 2.4.x <br><br>
+ 
+ <h3>Project Metadata </h3> 
+ - Group: hello <br>
+ - Artifact: item-service <br>
+ - Name: item-service <br>
+ - Package name: hello.itemservice <br> 
+ - Packaging: Jar <br> 
+ - Java: 17 <br><br>
+ 
+ <h3>Dependencies: Spring Web, Thymeleaf , Lombok  </h3><br>
+
+
+<h2> 프로젝트 화면 </h2>
+
+<h3>아이템 목록</h3>
+
+![image](https://github.com/user-attachments/assets/23a344ee-6d3b-4ee6-8422-99c751434b44)
+
+<h3>아이템 등록</h3>
+
+![image](https://github.com/user-attachments/assets/511c4f12-daa8-42f7-b3a9-aabadcbad9d1)
+
+<h3>아이템 정보</h3>
+
+![image](https://github.com/user-attachments/assets/720fbf77-979b-41ee-8616-3d3f301d155f)
+
---
+++ inflearn/김영한/스프링 MVC1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,146 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     *  V2
+     *  ModelAttribute
+     *  기능1. Item에 데이터를 담아줌. -> 요청 파라미터의 값을 프로퍼티 접근법으로 넣어줌
+     *  기능2. model.addAttribute("item", item); 를 자동으로 추가.
+     */
+
+    //    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+//      model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+
+    /**
+     *  ModelAttribute의 이름을 빼면
+     *  클래스 명의 첫 글자를 소문자로 바꿔서 ModelAttribute에 넣어준다.
+     *  Item -> item
+     */
+    //    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
+    /**
+     *  자신이 임의로 만든 클래스의 경우
+     *  ModelAttribute를 생략하더라도 적용이 된다.
+     */
+
+    //    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
+    //    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+
+    /**
+     *  redirectAttributes를 활용
+     *  - 인코딩 작업도 redirectAttributes가 대신해줌.
+     *  item.html에서 status 여부에 따라서 h2를 출력해준다.
+     */
+
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        // itemID가 들어가고 status의 경우에는 쿼리 파리미터로 들어감 ?status
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
+
