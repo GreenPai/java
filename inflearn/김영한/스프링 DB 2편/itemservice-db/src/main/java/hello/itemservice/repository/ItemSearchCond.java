be80dbf384d031b0d3b945171df7b30e248f33c6
GreenPai <clsrntkdgh@gmail.com>
2026-08-19 22:45:55 +0900

    DB2

--- inflearn/김영한/스프링 DB 2편/itemservice-db/src/main/java/hello/itemservice/repository/ItemSearchCond.java
+++ inflearn/김영한/스프링 DB 2편/itemservice-db/src/main/java/hello/itemservice/repository/ItemSearchCond.java
@@ -2,6 +2,10 @@ package hello.itemservice.repository;
 
 import lombok.Data;
 
+/**
+ * 검색 조건으로 사용한다는 규칙을 정했다.
+ * DTO로 붙여도되지만 Cond를 이용해서 이름으로서 추측을 할 수 있도록 했다.
+ */
 @Data
 public class ItemSearchCond {
 
